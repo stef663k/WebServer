12e2da78d4826be548f6eaeeed8806d41f5253d4
stef663k <stef663k@elev.techcollege.dk>
2023-04-27 08:01:22 +0200

    Not working HEAD

--- webServer.zig
+++ webServer.zig
@@ -2,7 +2,10 @@ const std = @import("std");
 const net = std.net;
 const StreamServer = net.StreamServer;
 const Address = net.Address;
+const GenerealPurposeAllocator = std.heap.GeneralPurposeAllocator;
 pub const io_mode = .evented;
+var gpa = GenerealPurposeAllocator(.{}){};
+const allocator = gpa.allocator();
 
 pub fn main() !void {
     var streamServer = StreamServer.init(.{});
@@ -17,6 +20,25 @@ pub fn main() !void {
 }
 
 fn handlerConnection(stream: net.Stream) !void {
+    _ = allocator;
     defer stream.close();
+    var first_line = stream.reader().readUntilDelimiterAlloc(allocator, '\n', std.math.maxInt(usize));
+    first_line = first_line[0..first_line.len];
+    var first_line_iter = std.mem.split(u8, first_line, "");
+
+    const method = first_line_iter.next();
+    _ = method;
+    const uri = first_line_iter.next();
+    _ = uri;
+    const version = first_line_iter.next();
+    _ = version;
+
+    while (true) {
+        var line = stream.reader().readUntilDelimiterAlloc(allocator, '\n', std.math.maxInt(usize));
+        line = line[0..line.len];
+
+        if (line.len == 2 and std.mem.eql(line, "\r\n")) break;
+    }
+
     try stream.writer().print("Hello world", .{});
 }
